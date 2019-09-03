d1c323bcaea41e4b8e971021a74fbdce1f995e68
Abhidnya Patil <abpati@microsoft.com>
2019-09-03 14:34:08 -0700

    Changing scope to User.Read

--- app_config.py
+++ app_config.py
@@ -1,5 +1,5 @@
 AUTHORITY = "https://login.microsoftonline.com/tenant-name"
 CLIENT_ID = "client-id-as-obtained-during-app-registration"
 CLIENT_SECRET = "client-secret-obtained-during-app-registration"
-SCOPE = ["https://graph.microsoft.com/.default"]
+SCOPE = ["https://graph.microsoft.com/User.Read"]
 REDIRECT_URI = "http://localhost:5000/getAToken"
