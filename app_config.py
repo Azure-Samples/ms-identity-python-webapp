aac6cf43f18ae2e26eb7b02f1f7e0df190151eb9
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-09-03 16:20:55 +0200

    Adding defaults

--- app_config.py
+++ app_config.py
@@ -1,6 +1,6 @@
-TENANT = ""
+TENANT = "tenant-name"
 AUTHORITY = "https://login.microsoftonline.com/"+ TENANT
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+CLIENT_ID = "client-id-as-obtained-during-app-registration"
+CLIENT_SECRET = "client-secret-obtained-during-app-registration"
 SCOPE = "https://graph.microsoft.com/.default"
 REDIRECT_URI = "http://localhost:5000/getAToken"
