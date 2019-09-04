dbf55ae21513c6f1a2c91b3d84574f6eb74ae72a
Abhidnya Patil <abpati@microsoft.com>
2019-09-04 11:00:29 -0700

    Adding comments for more information

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ class Config:
     """Set Flask configuration vars from .env file."""
 
     # General Config
-    SECRET_KEY = 'super_secret_key'
+    SECRET_KEY = 'super_secret_key'  # Setting it as an environment variable is recommended
 
     # Flask-Session
-    SESSION_TYPE = 'filesystem'
+    SESSION_TYPE = 'filesystem'  # You can choose between Redis, SqlAlchemy, MongoDB or memcachec
+    # There are other configuration options which can be used for a more customized application
+    # More information about this can be found here : https://pythonhosted.org/Flask-Session/
