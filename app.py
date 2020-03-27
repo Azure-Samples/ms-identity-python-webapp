5442849dc5234ebb854da4b3bb5cfc6bc4112392
Ray Luo <rayluo.mba@gmail.com>
2020-03-27 11:45:30 -0700

    Improvement with ProxyFix for deploying to production

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ app = Flask(__name__)
 app.config.from_object(app_config)
 Session(app)
 
+# This section is needed for url_for("foo", _external=True) to automatically
+# generate http scheme when this sample is running on localhost,
+# and to generate https scheme when it is deployed behind reversed proxy.
+# See also https://flask.palletsprojects.com/en/1.0.x/deploying/wsgi-standalone/#proxy-setups
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 @app.route("/")
 def index():
