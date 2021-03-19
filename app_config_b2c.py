e342e93a2a7e0cc4d4955c20660e6a81fd2536c5
Ray Luo <rayluo.mba@gmail.com>
2021-03-19 14:44:14 -0700

    Update app_config_b2c.py
    A temporary solution to document the deprecation of resetpassword_user_flow

--- app_config_b2c.py
+++ app_config_b2c.py
@@ -3,7 +3,12 @@ import os
 b2c_tenant = "fabrikamb2c"
 signupsignin_user_flow = "B2C_1_signupsignin1"
 editprofile_user_flow = "B2C_1_profileediting1"
-resetpassword_user_flow = "B2C_1_passwordreset1"
+
+resetpassword_user_flow = "B2C_1_passwordreset1"  # Note: Legacy setting.
+    # If you are using the new
+    # "Recommended user flow" (https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-versions),
+    # you can remove the resetpassword_user_flow and the B2C_RESET_PASSWORD_AUTHORITY settings from this file.
+
 authority_template = "https://{tenant}.b2clogin.com/{tenant}.onmicrosoft.com/{user_flow}"
 
 CLIENT_ID = "Enter_the_Application_Id_here" # Application (client) ID of app registration
@@ -20,8 +25,12 @@ AUTHORITY = authority_template.format(
     tenant=b2c_tenant, user_flow=signupsignin_user_flow)
 B2C_PROFILE_AUTHORITY = authority_template.format(
     tenant=b2c_tenant, user_flow=editprofile_user_flow)
+
 B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(
     tenant=b2c_tenant, user_flow=resetpassword_user_flow)
+    # If you are using the new
+    # "Recommended user flow" (https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-versions),
+    # you can remove the resetpassword_user_flow and the B2C_RESET_PASSWORD_AUTHORITY settings from this file.
 
 REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
                               # The absolute URL must match the redirect URI you set
