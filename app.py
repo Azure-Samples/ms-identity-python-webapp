d13d6912f4baad4612e3cabc5451c3624d17f2b1
Ray Luo <rayluo.mba@gmail.com>
2023-02-07 16:08:06 -0800

    Adopt latest identity 0.2.0

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import requests
 from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session  # https://pythonhosted.org/Flask-Session
-from werkzeug.exceptions import Unauthorized, Forbidden
-from identity import __version__
-from identity.web import Web, LifespanValidator
+import identity, identity.web
+import requests
 import app_config
 
 
@@ -18,50 +16,45 @@ Session(app)
 from werkzeug.middleware.proxy_fix import ProxyFix
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-web = Web(
+auth = identity.web.Auth(
     session=session,
     authority=app.config.get("AUTHORITY"),
     client_id=app.config["CLIENT_ID"],
     client_credential=app.config["CLIENT_SECRET"],
-    redirect_uri="http://localhost:5000" + app.config["REDIRECT_PATH"],  # It must match your redirect_uri
-    validators=[LifespanValidator(seconds=3600, on_error=Unauthorized("Login expired"))],
     )
-
-@app.route("/")
-def index():
-    if not web.get_user():
-        return redirect(url_for("login"))
-    return render_template('index.html', user=web.get_user(), version=__version__)
 
 @app.route("/login")
 def login():
-    return render_template("login.html", version=__version__, **web.start_auth(scopes=app_config.SCOPE))
+    return render_template("login.html", version=identity.__version__, **auth.log_in(
+        scopes=app_config.SCOPE,  # Have user consent scopes during log-in
+        redirect_uri=url_for("auth_response", _external=True),  # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
+        ))
 
-@app.errorhandler(Unauthorized)
-def handler(error):
-    return redirect(url_for("login"))
-
-@app.route(app_config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
+@app.route(app_config.REDIRECT_PATH)
 def auth_response():
-    result = web.complete_auth(request.args)
-    if "error" in result:
-        return render_template("auth_error.html", result=result)
-    return redirect(url_for("index"))
+    result = auth.complete_log_in(request.args)
+    return render_template("auth_error.html", result=result) if "error" in result else redirect(url_for("index"))
 
 @app.route("/logout")
 def logout():
-    return redirect(web.sign_out(url_for("index", _external=True)))
+    return redirect(auth.log_out(url_for("index", _external=True)))
 
-@app.route("/graphcall")
-def graphcall():
-    token = web.get_token(app_config.SCOPE)
-    if not token:
+@app.route("/")
+def index():
+    if not auth.get_user():
         return redirect(url_for("login"))
-    graph_data = requests.get(  # Use token to call downstream service
+    return render_template('index.html', user=auth.get_user(), version=identity.__version__)
+
+@app.route("/call_downstream_api")
+def call_downstream_api():
+    token = auth.get_token_for_user(app_config.SCOPE)
+    if "error" in token:
+        return redirect(url_for("login"))
+    api_result = requests.get(  # Use token to call downstream api
         app_config.ENDPOINT,
         headers={'Authorization': 'Bearer ' + token['access_token']},
         ).json()
-    return render_template('display.html', result=graph_data)
+    return render_template('display.html', result=api_result)
 
 if __name__ == "__main__":
     app.run()
