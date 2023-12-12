67e4a47f025648ee4dc09ff83ebc356fec96be43
Ray Luo <rayluo.mba@gmail.com>
2023-12-12 01:19:06 -0800

    Prompt to select account by default
    By adopting identity 0.3.1, this also fixes #121

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def login():
     return render_template("login.html", version=__version__, **auth.log_in(
         scopes=app_config.SCOPE, # Have user consent to scopes during log-in
         redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
+        prompt="select_account",  # Optional. More values defined in  https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
         ))
 
 
