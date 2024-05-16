499df39f427d039c11930d15e7691fa8948e7404
Ray Luo <rayluo@microsoft.com>
2024-05-15 18:01:27 -0700

    These were the demos that I planned to show in my presentation

--- app.py
+++ app.py
@@ -7,6 +7,33 @@ import app_config
 __version__ = "0.9.0"  # The version of this sample, for troubleshooting purpose
 
 app = Flask(__name__)
+
+# Test this with private browser at http://localhost:5000/foo
+@app.route("/foo")
+def foo():
+    return "foo"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.config.from_object(app_config)
 auth = Auth(
     app,
@@ -21,19 +48,60 @@ auth = Auth(
     b2c_reset_password_user_flow=os.getenv('RESETPASSWORD_USER_FLOW'),
 )
 
+# Test this with private browser at http://localhost:5000/foo
+# using johndoe@my_test_tenant.onmicrosoft.com
+@app.route("/bar")
+@auth.login_required()
+def bar(*, context):
+    return "bar"
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/")
 @auth.login_required
 def index(*, context):
     return render_template(
         'index.html',
-        user=context['user'],
+        user=context['user'],  # Variable user has a dict of the ID token claims
         edit_profile_url=auth.get_edit_profile_url(),
         api_endpoint=os.getenv("ENDPOINT"),
         title=f"Flask Web App Sample v{__version__}",
     )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Uncomment the SCOPE in .env
 @app.route("/call_api")
 @auth.login_required(scopes=os.getenv("SCOPE", "").split())
+#@auth.login_required(scopes=["Calendars.ReadWrite"],
 def call_downstream_api(*, context):
     api_result = requests.get(  # Use access token to call a web api
         os.getenv("ENDPOINT"),
